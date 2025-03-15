948212abdf775ef82f96223108bf5a66259527e0
YASH TANNA <yashtannasvkd@gmail.com>
2025-03-15 22:39:28 +0530

    Adding CORs

--- src/main/java/com/yash/blog/BlogAppApisApplication.java
+++ src/main/java/com/yash/blog/BlogAppApisApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BlogAppApisApplication {
@@ -16,5 +18,18 @@ public class BlogAppApisApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Applies to all endpoints
+                        .allowedOrigins("*") // Accepts requests from any domain
+                        .allowedMethods("*") // Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+                        .allowedHeaders("*"); // Accepts all headers
+            }
+        };
+    }
 
 }
